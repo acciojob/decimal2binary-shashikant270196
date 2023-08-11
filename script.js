d41c5c94b248117167c05caffeaa594a0c20f157
Shashikant yadav <127616023+shashikant270196@users.noreply.github.com>
2023-08-11 19:57:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function decimalToBinary(num) {
-  //Write you code here
+let bin = 0;
+    let rem, i = 1, step = 1;
+    while (num != 0) {
+        rem = num % 2;
+        console.log(
+            `Step ${step++}: ${num}/2, Remainder = ${rem}, Quotient = ${parseInt(num/2)}`
+        );
+        num = parseInt(num / 2);
+        bin = bin + rem * i;
+        i = i * 10;
   
 }
 
